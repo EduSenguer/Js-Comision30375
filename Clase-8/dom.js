116e614a341fc0c2af66f6465695f3ecfd22cbdf
EduSenguer <ledu1893@gmail.com>
2022-05-24 12:02:50 -0300

    clases 8-9-10

---
+++ Clase-8/dom.js
@@ -0,0 +1,8 @@
+//DOM
+console.dir(document.body.children);
+
+//getElementById
+let header = document.getElementById("header");
+console.log(header.innerHTML);
+
+header.style.background="lightgreen";
