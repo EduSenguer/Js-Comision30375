ea6a7430800e350c6ffd9294f9dbdedc8d9e7548
EduSenguer <ledu1893@gmail.com>
2022-06-09 23:12:20 -0300

    SweetAlert-Entrega

---
+++ Entregas/Entrega-4-Librerias/figuras.js
@@ -0,0 +1,66 @@
+const figuras = [
+    {
+        id: 1,
+        name: "Man-Spider",
+        imagen: "./img/Man-Spider.jpeg",
+        group: "Spider Man Classics",
+        Year: "Year: 2000",
+        Price: 95
+    },
+    {
+        id: 2,
+        name: "SpiderMan",
+        imagen: "./img/SpiderMan.jpeg",
+        group: "Spider Man Classics",
+        Year: "Year: 2000",
+        Price: 55
+    },
+    {
+        id: 3,
+        name: "Simbionte",
+        imagen: "./img/Simbionte.jpeg",
+        group: "Spider Man Classics",
+        Year: "Year: 2000",
+        Price: 62
+    },
+    {
+        id: 4,
+        name: "Venom",
+        imagen: "./img/venom-4206.jpeg",
+        group: "Spider Man Classics",
+        Year: "Year: 2000",
+        Price: 51
+    },
+    {
+        id: 5,
+        name: "Daredevil",
+        imagen: "./img/Daredevil.jpeg",
+        group: "Spider Man Classics",
+        Year: "Year: 2001",
+        Price: 22
+    },
+    {
+        id: 6,
+        name: "Rhino",
+        imagen: "./img/Rhino.jpeg",
+        group: "Spider Man Classics",
+        Year: "Year: 2001",
+        Price: 36
+    },
+    {
+        id: 7,
+        name: "Scarlet-Spider",
+        imagen: "./img/Scarlet-Spider.jpeg",
+        group: "Spider Man Classics",
+        Year: "Year: 2001",
+        Price: 147
+    },
+    {
+        id: 8,
+        name: "SpiderMan 2099",
+        imagen: "./img/SpiderMan 2099.jpeg",
+        group: "Spider Man Classics",
+        Year: "Year: 2001",
+        Price: 449
+    }
+];
